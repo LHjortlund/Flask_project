95805dd2d3e57773f4c4e1a157414152ef40a59a
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-10-13 22:05:19 +0200

    speed

--- decorater.py
+++ decorater.py
@@ -1,20 +1,53 @@
 import time
 #Decorater function
+#
+# def delay_decorator_function(func):
+#     def wrapper_func():
+#         time.sleep(2)
+#         #Do something before
+#         func()
+#         func()
+#         #do something after
+#     return wrapper_func
+#
+# @delay_decorator_function
+# def say_hello():
+#     print("Hello World!")
+#
+# def say_goodbye():
+#     print("Bye")
+#
+# def say_greeting():
+#     print("How are you")
+#
+# say_greeting()
 
-def delay_decorator_function(func):
-    def wrapper_func():
-        func()
-    return wrapper_func
+#other challenge:
+import time
 
-@delay_decorator_function
-def say_hello():
-    time.sleep(2)
-    print("Hello World!")
 
-def say_goodbye():
-    print("Bye")
+def speed_calc_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"{func.__name__} run speed: {end_time - start_time}s")
+        return result
 
-def say_greeting():
-    print("How are you")
+    return wrapper
 
-say_hello()
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(1000000):
+        i * i
+
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(10000000):
+        i * i
+
+
+fast_function()
+slow_function()
