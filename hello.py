713e50f6c60bfe3b0dfe03226d73a7bf10223e25
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-10-14 10:52:44 +0200

    import img and gif

--- hello.py
+++ hello.py
@@ -6,12 +6,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Learning Flask</h1><p>Learn More Be Smart</p>'
-
+    return ('<h1 style="text-align: center">Learning Flask</h1>'
+            '<p>Learn More Be Smart</p>'
+            '<img src="https://d2zp5xs5cp8zlg.cloudfront.net/image-61785-800.jpg">')
 
 @app.route('/about')
 def about():
-    return '<h3>Now we are learning about flask</h3>'
+    return ('<h3>Now we are learning about flask</h3>'
+            '<iframe src="https://giphy.com/embed/Ow7TbhjmovnmhBomuN" width="480" height="480" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/halloween-creepy-spooky-Ow7TbhjmovnmhBomuN">via GIPHY</a></p>')
 
 
 @app.route('/contact')
